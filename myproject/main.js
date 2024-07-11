c8bc5cae97cc9fd2547d418835bb9861320fba29
positiveoo1 <github0512@gmail.com>
2024-07-11 11:41:57 +0200

    updated

--- myproject/main.js
+++ myproject/main.js
@@ -45,8 +45,8 @@ renderer.setPixelRatio(devicePixelRatio)
  camera.position.z = 5;
 
  const planeGeometry = new THREE.PlaneGeometry(5,4,10,10);
- const planeMeshMaterial = new THREE.MeshPhongMaterial({color: 0xff0000, side:THREE.DoubleSide,
-  flatShading: true  });
+ const planeMeshMaterial = new THREE.MeshPhongMaterial({ side:THREE.DoubleSide,
+  flatShading: true , vertexColors: true });
  const planeMesh = new THREE.Mesh(planeGeometry, planeMeshMaterial);
  scene.add(planeMesh);
  const {array} = planeMesh.geometry.attributes.position
@@ -68,6 +68,12 @@ addEventListener("mousemove", (e) => {
   
 })
 
+const colors = [];
+for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+  colors.push(1,0,0)
+}
+planeMesh.geometry.setAttribute("color", new THREE.BufferAttribute(new Float32Array(colors), 3))
+
 
 const light = new THREE.DirectionalLight(0xffffff ,1 );
 light.position.set(0,0,1);
@@ -81,6 +87,18 @@ scene.add(backLight);
   requestAnimationFrame(animte);
  renderer.render(scene, camera);
   // planeMesh.rotation.x += 0.01;
+
   rayCaster.setFromCamera(mouse, camera);
+  const intersects = rayCaster.intersectObject(planeMesh);
+  if(intersects.length > 0) {
+
+    const {color} = intersects[0].object.geometry.attributes;
+    color.setX(intersects[0].face.a, 0)
+    color.setY(intersects[0].face.a, 1)
+    color.setX(intersects[0].face.a, 0)
+    color.setX(intersects[0].face.a, 0)
+    
+    intersects[0].object.geometry.attributes.color.needsUpdate = true;
+  }
 }
  animte();
